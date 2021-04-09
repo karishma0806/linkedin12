511dd0c869cb11fd1eef2d1ec1a44af464cd86e6
karishma0806 <70712631+karishma0806@users.noreply.github.com>
2021-04-09 20:19:48 +0530

    Add files via upload

---
+++ linkedin.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+import requests
+
+
+# In[20]:
+
+
+from bs4 import BeautifulSoup
+
+
+# In[21]:
+
+
+import selenium
+
+
+# In[22]:
+
+
+import time
+
+
+# In[23]:
+
+
+from selenium import webdriver
+
+
+# In[24]:
+
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+# In[41]:
+
+
+browser  = webdriver.Chrome(ChromeDriverManager().install())
+
+
+# In[42]:
+
+
+browser.get('https://www.linkedin.com')
+
+
+# In[43]:
+
+
+browser.get('https://www.linkedin.com/in/syed-karishma-73bba11a0')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
